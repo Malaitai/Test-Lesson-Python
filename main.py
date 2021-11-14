6fd1da867a92f84eb7f80e759e9b73b0695bae0b
Mr Malaitai (School) <49211523+Malaitai@users.noreply.github.com>
2021-11-15 08:28:45 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 # Task 1: Print "Kia ora, World!"
 print("")
 
-# Task 2: Ask for name and print "Hello..." followed by their name
+# Task 2: Ask for name and print "Hello..." followed by their name, e.g. "Hello John"
