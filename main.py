4024ae1b989f3353bedf8c0224ea7e16a2403c95
Mr Malaitai (School) <49211523+Malaitai@users.noreply.github.com>
2021-11-15 08:44:14 +1300

    Update and rename main.py to task_one.py

--- main.py
+++
@@ -1,4 +0,0 @@
-# Task 1: Print "Kia ora, World!"
-print("")
-
-# Task 2: Ask for name and print "Hello..." followed by their name, e.g. "Hello John"
---
+++ task_one.py
@@ -0,0 +1,2 @@
+# Task 1: Print "Kia ora, World!"
+print("")
