71db2c71faeceaac76a8060b26682442678c3fba
Mr Malaitai (School) <49211523+Malaitai@users.noreply.github.com>
2021-11-15 08:13:27 +1300

    Update and rename test.py to main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+# Task 1: Print hello world
+print("")
+
+# Task 2: Ask for name and print "Hello..." followed by their name
--- test.py
+++
@@ -1,4 +0,0 @@
-# Print hello world
-print("")
-
-# Ask for name and print "Hello..." followed by their name
